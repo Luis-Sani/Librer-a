68e5550d23189b43887c6bc9acd479e3251e552f
JDanielCaler0 <136024397+JDanielCaler0@users.noreply.github.com>
2024-03-14 20:32:16 -0500

    Add files via upload

---
+++ Libreria v2/Services/ServicioUsuario.cs
@@ -0,0 +1,37 @@
+﻿using Libreria.Models;
+using Libreria.Models.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libreria.Services
+{
+    public class ServicioUsuario : IServicioUsuario
+    {
+        private readonly LibreriaContext _context;
+
+        public ServicioUsuario(LibreriaContext context)
+        {
+            _context = context;
+        }
+        public async Task<Usuario> GetUsuario(string correo, string clave)
+        {
+            Usuario usuario = await _context.Usuarios.Where(u => u.Correo == correo && u.Clave == clave)
+               .FirstOrDefaultAsync();
+
+            return usuario;
+        }
+
+        public async Task<Usuario> GetUsuario(string nombreUsuario)
+        {
+            return await _context.Usuarios
+                .FirstOrDefaultAsync(u => u.NombreUsuario == nombreUsuario);
+
+        }
+
+        public async Task<Usuario> SaveUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+            return usuario;
+        }
+    }
+}
