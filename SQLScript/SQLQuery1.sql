35e9104bbe95ce276b1adde91957f03762644209
JDanielCaler0 <136024397+JDanielCaler0@users.noreply.github.com>
2024-03-14 21:07:54 -0500

    Add files via upload

---
+++ SQLScript/SQLQuery1.sql
@@ -0,0 +1,17 @@
+--tabla autores--
+
+insert into autores (Nombre, FechaNacimiento) values ('J.K. Rowling', '1965-07-31');
+insert into autores (Nombre, FechaNacimiento) values ('Stephen King', '1947-09-21');
+insert into autores (Nombre, FechaNacimiento) values ('Gabriel García Márquez', '1927-03-06');
+insert into autores (Nombre, FechaNacimiento) values ('Paulo Coelho', '1947-08-24');
+insert into autores (Nombre, FechaNacimiento) values ('Jorge Luis Borges', '1899-08-24');
+insert into autores (Nombre, FechaNacimiento) values ('Miguel de Cervantes', '1547-09-29');
+insert into autores (Nombre, FechaNacimiento) values ('Antoine de Saint-Exupéry', '1900-06-29');
+insert into autores (Nombre, FechaNacimiento) values ('Mark Twain', '1835-11-30');
+insert into autores (Nombre, FechaNacimiento) values ('Charles Dickens', '1812-02-07');
+insert into autores (Nombre, FechaNacimiento) values ('Jane Austen', '1775-07-28');
+insert into autores (Nombre, FechaNacimiento) values ('Leo Tolstoy', '1828-09-09');
+insert into autores (Nombre, FechaNacimiento) values ('Fiódor Dostoyevski', '1821-11-11')
+insert into autores (Nombre, FechaNacimiento) values ('Ernest Hemingway', '1899-07-21');
+insert into autores (Nombre, FechaNacimiento) values ('Albert Camus', '1913-11-07');
+insert into autores (Nombre, FechaNacimiento) values ('William Shakespeare', '1564-04-26');
