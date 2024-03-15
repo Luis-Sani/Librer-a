cd004e841224a3d58d4f675cb74d6a38d51c13a5
JDanielCaler0 <136024397+JDanielCaler0@users.noreply.github.com>
2024-03-14 20:30:31 -0500

    Add files via upload

---
+++ Libreria v2/Models/Entidades/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libreria.Models.Entidades
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio. ")]
+
+        public string Nombre { get; set; }
+
+        [DataType(DataType.MultilineText)]
+
+        public string Descripcion { get; set; }
+    }
+}
