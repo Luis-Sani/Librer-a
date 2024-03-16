a4053e10e6d3651ad075e72aa72db9ee21e02a1f
Luis-Sani <135286261+Luis-Sani@users.noreply.github.com>
2024-03-15 21:32:31 -0500

    Add files via upload

---
+++ Libreria FINAL/Models/Entidades/Autor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace Libreria.Models.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+
+        public string Nombre { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0: yyyy/MM/dd hh:mm tt}")]
+
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
